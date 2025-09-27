d7a10abcadc3eb8c227ad62326d7310312ee622c
huy-lehoangnhat-maersk <huy.le@lns.maersk.com>
2025-09-27 23:14:56 +0700

    draft

--- src/task/web/SCP_upload.py
+++ src/task/web/SCP_upload.py
@@ -11,13 +11,14 @@ class SCP_upload(WebTask):
 
     def __init__(self, settings: dict[str, str], callback_before_run_task: Callable[[], None]):
         super().__init__(settings, callback_before_run_task)
+        self._document_folder = self._download_folder
 
     def mandatory_settings(self) -> list[str]:
         mandatory_keys: list[str] = ['username', 'password', 'download.folder', 'excel.path', 'excel.sheet',
                                      'excel.column.bill']
         return mandatory_keys
 
-    def automate(self) -> None:
+    def automate(self):
         logger: Logger = get_current_logger()
         logger.info(
             "---------------------------------------------------------------------------------------------------------")
@@ -27,7 +28,9 @@ class SCP_upload(WebTask):
         logger.info('Try to login')
         self.__login()
 
-        self._try_to_get_if_element_present(by=By.CSS_SELECTOR, value='anc')
+        key = 'msal.idtoken'
+        value = self._driver.execute_script(f"return window.localStorage.getItem('{key}');")
+        logger.info('Completed automation')
 
     def __login(self) -> None:
         username: str = self._settings['username']
@@ -40,3 +43,12 @@ class SCP_upload(WebTask):
 
         self._type_when_element_present(by=By.ID, value='i0118', content=password)
         self._click_when_element_present(by=By.ID, value='idSIButton9')
+
+        while True:
+            url_expect = self._driver.current_url
+            if 'scm-preprod' in url_expect:
+                break
+
+            if 'login' in url_expect:
+                self.sleep()
+                print('sleep')
